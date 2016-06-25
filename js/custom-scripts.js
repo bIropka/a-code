188ed2233aa02bae0501e3a1ab0f3fd175473532
Iegor Stetsiura <biropka@gmail.com>
2016-06-26 01:04:20 +0300

    final

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -25,4 +25,26 @@ $(document).ready(function () {
         }
     });
 
+    $('.sign-item').click(function() {
+        $('.sign-item').removeClass('active');
+        $(this).addClass('active');
+        if($(this).hasClass('sign-author')) {
+            $('.form-sign-in').fadeIn();
+            $('.form-password-recovery').fadeOut(0);
+        } else {
+            $('.form-sign-in').fadeOut(0);
+            $('.form-password-recovery').fadeIn();
+        }
+    });
+
+    $('.sign-up').click(function () {
+        $('.window-sign').fadeIn();
+    });
+
+    $('.window-sign').click(function (event) {
+        $target = $(event.target);
+        if (!$target.closest($('.window-inner-sign')).length) $('.window-sign').fadeOut();
+        if ($target.hasClass('close-marker')) $('.window-sign').fadeOut();
+    });
+
 });
